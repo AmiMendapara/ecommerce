71b27d2457df276130acbb8185980d1ecc49dbf2
AmiMendapara <amipatel42429@gmail.com>
2024-04-02 16:06:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App=()=>{
   };
   return(
     <ThemeProvider theme={theme}>
-    <Router basename="/tothepoint_login">
+    <Router basename={process.env.PUBLIC_URL}>
       <GlobalStyle/>
       <Header/>
       <Routes>
